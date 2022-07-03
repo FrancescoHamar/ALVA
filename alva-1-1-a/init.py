da1c8035fbded0e7c8cacd2767303b409326b56e
FrancescoHamar <francesco.hamar@gmail.com>
2022-07-03 16:09:47 +0200

    Initialized Alva-s1-v1-Alpha

--- alva-1-1-a/init.py
+++ alva-1-1-a/init.py
@@ -1,13 +1,26 @@
 import json
+from datetime import datetime
 
 
 def awake():
     with open("deephippo/tag.json") as tag:
         tag_data = json.load(tag)
-        return tag_data["born"]
+        if not tag_data["born"]:
+            birth()
+        else:
+            print("Already Alive :)")
+            # main()
 
 
 def birth():
-    
+    with open("deephippo/tag.json") as tag:
+        tag_data = json.load(tag)
+        tag_data["born"] = True
+        now = str(datetime.now())
+        tag_data["birthdate"] = now
+
+    with open("deephippo/tag.json", 'w') as tag:
+        json.dump(tag_data, tag)
+
 
 awake()
